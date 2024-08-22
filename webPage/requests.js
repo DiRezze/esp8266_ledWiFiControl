494a6bc438e5366c448ad1b4e6f791d498feac5a
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-08-22 09:46:10 -0300

    XMLHttp request

--- webPage/requests.js
+++ webPage/requests.js
@@ -1,25 +1,32 @@
 //Endereço IP do módulo ESP8266
 const ESP8266_IP = "";
 
-var led_controle = function(status) {
-    var xmlhttp = new XMLHttpRequest();
-    var set_status = "http://" + ESP8266_IP + "/?function=" + status;
-    xmlhttp.open("GET", set_status);
+//Estado incial do led embutido; nesse caso, definido pelo sistema embarcado como HIGH(apagado)
+let builtInLed_state = false;
+
+const builtLedControl = () => {
+    const xmlhttp = new XMLHttpRequest();
+    const status = builtInLed_state ? "builtLed_off" : "builtLed_on";
+    const setStatus = `http://${ESP8266_IP}/?function=${status}`;
+    xmlhttp.open("GET", setStatus, true);
+
+    xmlhttp.onreadystatechange = () => {
+        if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
+            builtInLed_state = !builtInLed_state;
+
+            const builtLedButton = document.getElementById('builtLedToggle');
+            builtLedButton.textContent = builtInLed_state ? 'Desligar' : 'Ligar';
+        }
+    }
+
     xmlhttp.send();
 }
 
-var builtInLed = function() {
-    var ligar = document.querySelector(".on");
-    ligar.onclick = function() {
-        led_controle("builtLed_on");
-    }
-
-    var desligar = document.querySelector(".off");
-    desligar.onclick = function() {
-        led_controle("builtLed_off");
-    }
+const builtInLed = () => {
+    var builtLedButton = document.getElementById('builtLedToggle');
+    builtLedButton.addEventListener("click", builtLedControl)
 }
 
-window.onload = function() {
+window.onload = () => {
     builtInLed();
 }
